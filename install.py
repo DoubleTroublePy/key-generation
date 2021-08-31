ca1a3915965384523c6d7af15116b964d48cbba0
DoubleTroublePy <73990389+DoubleTroublePy@users.noreply.github.com>
2021-08-31 14:19:34 +0200

    Update install.py

--- install.py
+++ install.py
@@ -3,6 +3,9 @@ os.system('rm keyGenerate.py')
 os.system('rm convertClass.py ')
 os.system('rm arguments.txt')
 
-os.system('wget https://raw.githubusercontent.com/DoubleTroublePy/key-generation/main/test.py > keyGenerate.py')
+os.system('wget https://raw.githubusercontent.com/DoubleTroublePy/key-generation/main/test.py')
+os.system('cp test.py keyGenerate.py')
+os.system('rm test.py')
+
 os.system('wget https://raw.githubusercontent.com/DoubleTroublePy/key-generation/main/convertClass.py')
 os.system('wget https://raw.githubusercontent.com/DoubleTroublePy/key-generation/main/arguments.txt')
