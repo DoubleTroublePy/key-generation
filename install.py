a108590072ba913a58e21e50441139c01f967768
DoubleTroublePy <73990389+DoubleTroublePy@users.noreply.github.com>
2021-08-31 13:50:51 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,3 @@
+exec('wget https://raw.githubusercontent.com/DoubleTroublePy/key-generation/main/test.py > keyGenerate.py')
+exec('wget https://raw.githubusercontent.com/DoubleTroublePy/key-generation/main/convertClass.py > convertClass.py ')
+exec('wget https://raw.githubusercontent.com/DoubleTroublePy/key-generation/main/arguments.txt > arguments.txt')
